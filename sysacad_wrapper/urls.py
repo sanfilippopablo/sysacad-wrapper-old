e941ece81f8820c2598e69bb92ba701e6af62265
Pablo Sanfilippo <giamgreeg@gmail.com>
2013-11-16 03:35:10 -0300

    Más sobre estructura básica del proyecto.

--- sysacad_wrapper/urls.py
+++ sysacad_wrapper/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from website.views import HomeView
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sysacad_wrapper.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+     url(r'^$', HomeView.as_view(), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
