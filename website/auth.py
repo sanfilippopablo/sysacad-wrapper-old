ab1573cd8327339c9a260c5d5e451b2b27e7d186
Pablo Sanfilippo <giamgreeg@gmail.com>
2013-11-18 11:04:06 -0300

    Some work on auth system.

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,32 @@
 from django import models
+from sysacad_api import SysacadSession
 
 class SysacadAuthBackend(object):
 	def authenticate(self, fr=None, legajo=None, password=None):
-		# Aca se hace lo que escribi en el CUU.
-		pass
+		s = SysacadSession(fr, legajo, password)
+		try:
+			s.login()
+		except:
+			return None
+		try:
+			alumno = Alumno.objects.get(fr=fr, legajo=legajo)
+		except Alumno.DoesNotExist:
+			return Alumno.objects.create_user(fr=fr, legajo=legajo, password=password)
+		else:
+			if check_password(password, alumno.password):
+				return alumno
 
 	def get_user(self, user_id):
-		pass
+		try:
+			Alumno.objects.get(pk=user_id)
+		except Alumno.DoesNotExist:
+			return None
 
-class SysacadAlumnoManager(models.BaseUserManager):
+class AlumnoManager(models.BaseUserManager):
+
+	# TODO: Ir fijandome en el django sc los metodos de BaseUserManager que
+	# no sirve e ir redefiniéndolos.
+
 	def create_user(self, fr, legajo, password=None):
 		pass
 
@@ -20,7 +38,13 @@ frs = (
 	('frre', 'Resistencia'),
 )
 
-class Alumno(models.Model):
+class Alumno(AbstractBaseUser):
+
+	# TODO: Ir fijandome en el django sc los metodos de AbstractBaseUser que
+	# no sirve e ir redefiniéndolos.
+
 	fr = models.CharField(max_length=10, choices=frs)
 	legajo = models.CharField(max_length=30)
-	password = models.CharField(max_length=100)
+	password = models.CharField(max_length=128)
+
+	objects = AlumnoManager()
