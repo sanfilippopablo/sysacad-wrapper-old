8f0e788370116aa0414a2f30a7713e09e1e7a235
Pablo Sanfilippo <giamgreeg@gmail.com>
2013-11-20 00:56:18 -0300

    First work with templates. base.html.

--- sysacad_wrapper/urls.py
+++ sysacad_wrapper/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-
+    url(r'^$', TemplateView.as_view(template_name='dashboard.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
