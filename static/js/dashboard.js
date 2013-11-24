e6ce5007b1bf6c05174f5aa96a661bb4e259b4a1
Pablo Sanfilippo <giamgreeg@gmail.com>
2013-11-23 23:19:50 -0300

    Ahora el sistema se entera que se están actualizando los datos.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,1 +1,22 @@
-console.log("Hello World.")
+function getCookie(name) {
+  var parts = document.cookie.split(name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+$(document).ready(function() {
+	$.ajax({
+	    url : '/ajax/dashboard_data/',
+	    type: 'POST',
+	    headers: {'X-CSRFToken':getCookie('csrftoken')
+	        ,'sessionid':getCookie('sessionid')
+	        },
+	    success : function(data) {
+	        if (data == 'password_required') {
+	        	console.log('Password required.');
+	        } else {
+	        	$('#content-container').html(data);
+	        	$('.actualizando-alert').removeClass('active');
+	        };
+	    }
+	});
+});
--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,6 @@
-function getCookie(name) {
-  var parts = document.cookie.split(name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
-}
-
 $(document).ready(function() {
 	// Off-Canvas
  	$('[data-toggle=offcanvas]').click(function() {
     	$('.row-offcanvas').toggleClass('active');
   	});
-
-	$.ajax({
-	    url : '/ajax/dashboard_data/',
-	    type: 'POST',
-	    headers: {'X-CSRFToken':getCookie('csrftoken')
-	        ,'sessionid':getCookie('sessionid')
-	        },
-	    success : function(data) {
-	        if (data == 'password_required') {
-	        	console.log('Password required.');
-	        } else {
-	        	$('#content-container').html(data);
-	        	console.log('New data inyected.')
-	        };
-	    }
-	});
 });
