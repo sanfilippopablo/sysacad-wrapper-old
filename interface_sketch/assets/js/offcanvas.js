554b13d90a2733157ed7cd47c17da0a6d4b54fb3
Pablo Sanfilippo <giamgreeg@gmail.com>
2013-12-02 21:23:07 -0300

    Eliminada la carpeta interface_sketch.

--- interface_sketch/assets/js/offcanvas.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
-});
-
-$('.progress-bar').tooltip()
-
-$('#notificaciones').popover()
